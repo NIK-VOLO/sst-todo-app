c4e1bf9b6b3be34b00dfe1ccd655aabcd1698f08
NIK-VOLO <61594892+NIK-VOLO@users.noreply.github.com>
2023-01-25 20:12:10 -0500

    Added basic HTML and CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">TODO List</div>
+      <div className="App-todo-list">
+        <div className="App-input-container">
+          <form className="App-todo-input">
+            <label htmlFor="input">Input a new task</label>
+            <input type="text" className="input" id="input" />
+          </form>
+          <div>
+            <button className="btn App-add-button">Add</button>
+          </div>
+        </div>
+      </div>
+      <div className="App-todo-list">
+        <div className="App-todo-row">
+          <div className="App-todo-item">asdfasf</div>
+          <div>
+            <button className="btn App-delete-button">Remove</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
